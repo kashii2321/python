cd99e76b0709a62698cc8f336aefbe7908ad62b5
Kashvi Gupta <skashvi2309@gmail.com>
2025-04-10 20:08:11 +0530

    changed

--- VSCode/variables and data types/problemsetpy/pq1.py
+++ VSCode/variables and data types/problemsetpy/pq1.py
@@ -1,11 +1,11 @@
 # # write a code to input two numbers and print their sum
-# num1 = int(input("Num1 : "))
-# num2 = int(input("Num2 : "))
-# print("The sum is : ", num1 + num2)
+num1 = int(input("Num1 : "))
+num2 = int(input("Num2 : "))
+print("The sum is : ", num1 + num2)
 
-# #write a program to input side of square and calculate its area
-# side = int(input("The side is : "))
-# print("The area is : ", side**2)
+#write a program to input side of square and calculate its area
+side = int(input("The side is : "))
+print("The area is : ", side**2)
 
 # write a program to input 2 float numbers and to print their avg
 number1 = float(input("Enter the number : "))
