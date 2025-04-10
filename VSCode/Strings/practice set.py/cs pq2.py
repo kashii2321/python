289d09a79e81eff4d4337d6cb1eea4bd95e07f39
Kashvi Gupta <skashvi2309@gmail.com>
2025-04-10 20:16:49 +0530

    cs

--- VSCode/Strings/practice set.py/cs pq2.py
+++ VSCode/Strings/practice set.py/cs pq2.py
@@ -1,1 +1,10 @@
 # Write a program to find the greatest of 3 numbers entered by the user
+a = int(input("Enter the number : "))
+b = int(input("Enter the number : "))
+c = int(input("Enter the number : "))
+if(a >= b and a >= c):
+    print("a is the greatest")
+elif(b >= c):
+    print("b is the greatest")
+else:
+    print("c is the greatest")
---
+++ VSCode/Strings/practice set.py/cs pq3.py
@@ -0,0 +1,6 @@
+# write a program to check if the entered number is a multiple of 7 or not
+a = int(input("Enter the number : "))
+if(a%7 == 0):
+    print("Multiple of 7")
+else:
+    print("not a multiple of 7")
