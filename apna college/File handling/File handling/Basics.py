6a097684bdec3a44d9a2c5c4c46a41e11e1aa1a0
Kashvi Gupta <skashvi2309@gmail.com>
2025-04-13 23:54:26 +0530

    delete

--- apna college/File handling/File handling/Basics.py
+++
@@ -1,9 +0,0 @@
-# File I/O - I/O means input and output
-# files ke upr operations perform krne ko file I/O kehte h
-# jaise file ko read krna ya writing data 
-
-# types of File :
-# 1. Text files - jinke andr data is stored in form of character
-# .txt, .docx, .log
-# 2. Binary files - jo non text files hoti 
-# .mp4, .mov, .png, .jpeg etc 
